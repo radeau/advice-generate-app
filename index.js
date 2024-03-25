acb9be0daf9d04dc632c906c38abd0028c2478d0
radeau <rad_kali@Rad.>
2024-03-25 23:40:48 +0400

    Build: Add a button to generate new advice

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 function generateQuote() {
     fetch("https://api.adviceslip.com/advice")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then(data => {
-        const idNumber = data.slip.id;
-        const quote = data.slip.advice;
-        document.getElementById("idNumber").innerHTML = `"Advice #${idNumber}"`;
-        document.getElementById("quoteText").innerHTML = `"${quote}"`;
-      })
-      .catch(error => {
-        console.error("There was a problem fetching the quote:", error);
-      });
-  }
+    .then(response => {
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
+    }
+    return response.json();
+    })
+    .then(data => {
+    const idNumber = data.slip.id;
+    const quote = data.slip.advice;
+    document.getElementById("idNumber").innerHTML = `"Advice #${idNumber}"`;
+    document.getElementById("quoteText").innerHTML = `"${quote}"`;
+    })
+    .catch(error => {
+    console.error("There was a problem fetching the quote:", error);
+    });
+}
 
-  document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     generateQuote();
-  });
+});
+
+//Add button to generate new quote when clicked
+document.getElementById("generateNewButton").addEventListener("click", function(event) {
+    generateQuote();
+});
 
